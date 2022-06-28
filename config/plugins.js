c94e4705d979d9d2c3a8cf6bebd2d256380ec8ce
sherazmaker <sheraz.q@hashmakersol.net>
2022-06-28 18:43:53 +0500

    added env for cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
   upload: {
     provider: "cloudinary",
     providerOptions: {
-      cloud_name: env("CLOUDINARY_NAME"),
-      api_key: env("CLOUDINARY_KEY"),
-      api_secret: env("CLOUDINARY_SECRET"),
+      cloud_name: env("CLOUDINARY_NAME", "duxee2do5"),
+      api_key: env("CLOUDINARY_KEY", "432344255373111"),
+      api_secret: env("CLOUDINARY_SECRET", "L84pWlSb9bMvv3ykhax4iABpaqk"),
     },
     actionOptions: {
       upload: {},
