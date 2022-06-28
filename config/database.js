cb3b70e44ec9edfa41a73228f3167925cee45e1e
sherazmaker <sheraz.q@hashmakersol.net>
2022-06-28 16:07:49 +0500

    for heroku 22

---
+++ config/database.js
@@ -0,0 +1,47 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "mongoose",
+//       settings: {
+//         uri: env("DATABASE_URI"),
+//       },
+//       options: {
+//         ssl: true,
+//       },
+//     },
+//   },
+// });
+
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "mongoose",
+      settings: {
+        client: "mongo",
+        // uri: "mongodb+srv://logomoto:logomoto@cluster0.cs9j1.mongodb.net/?retryWrites=true&w=majority",
+        uri: "mongodb+srv://logomoto:logomoto@cluster0.cs9j1.mongodb.net/logomoto?retryWrites=true&w=majority",
+      },
+      options: {
+        ssl: true,
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4a938bfb-dcf0-46c4-b689-596d812091b0'
+};
